bf3ce38599cc82f2a4265f9d2fa87bcd7430b661
Sundaresang <sundarg0000@gmail.com>
2021-06-19 18:41:48 +0530

    Succesful program completed

---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# File guessinggame.sh
+echo "Enter your guess for number of files in the current directory"
+read guess
+while [[ $guess =~ .*[^0-9].* ]]
+do
+	echo "Please enter only numeric integer values"
+	read guess
+done
+actual=$(ls | wc -w)
+# echo $actual
+while [[ $guess -ne $actual ]]
+do
+	if [[ $guess -gt $actual ]]
+	then
+		echo "High. Please enter lower value"
+		read guess
+	else
+		echo "Low. Please enter higher value"
+		read guess
+	fi
+done
+echo "Congrats you have guessed the right value $actual"
+
